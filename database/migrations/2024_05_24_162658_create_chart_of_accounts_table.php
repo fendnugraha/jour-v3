effa71d339684465ff174d3c95810316eb951048
fendnugraha <fendnugraha92@gmail.com>
2024-05-24 23:44:42 +0700

    first commit

---
+++ database/migrations/2024_05_24_162658_create_chart_of_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chart_of_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('acc_code', 30)->unique()->index();
+            $table->string('acc_name', 100)->unique()->index();
+            $table->foreignId('account_id')->index();
+            $table->bigInteger('st_balance')->default(0);
+            $table->integer('warehouse_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chart_of_accounts');
+    }
+};
