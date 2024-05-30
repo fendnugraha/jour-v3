40f4ebf314f47a0e9964c0688b0d28b9500560c6
fendnugraha <fendnugraha92@gmail.com>
2024-05-31 02:50:50 +0700

    daily baru sampai mutasi saldo dan vocer

--- app/Livewire/Report/SoldVoucherTable.php
+++ app/Livewire/Report/SoldVoucherTable.php
@@ -2,12 +2,37 @@
 
 namespace App\Livewire\Report;
 
+use Carbon\Carbon;
+use App\Models\Sale;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class SoldVoucherTable extends Component
 {
+    use WithPagination;
+
+    public $search;
+    public $searchGroup;
+
     public function render()
     {
-        return view('livewire.report.sold-voucher-table');
+        $startDate = Carbon::now()->startOfDay();
+        $endDate = Carbon::now()->endOfDay();
+
+        $userWarehouseId = Auth()->user()->warehouse_id;
+
+        $sales = Sale::with('product')
+            ->whereBetween('date_issued', [$startDate, $endDate])
+            ->where('warehouse_id', $userWarehouseId)
+            ->whereHas('product', function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%');
+            })->paginate(5);
+
+        $salesGroup = $sales->groupBy('product_id');
+
+        return view('livewire.report.sold-voucher-table', [
+            'sales' => $sales,
+            'salesGroup' => $salesGroup,
+        ]);
     }
 }
--- resources/views/livewire/report/sold-voucher-table.blade.php
+++ resources/views/livewire/report/sold-voucher-table.blade.php
@@ -1,48 +1,71 @@
-<div class="container grid grid-cols-2 gap-3 mt-12">
-    <div>
-        <h4>Penambahan Saldo dari pusat</h4>
-        <input type="text" wire:model="search" placeholder="Search .." class="w-full border rounded-lg p-2 mb-1">
+<div class="container grid grid-cols-2 gap-3 mb-6">
+    <div class="bg-white p-2 rounded-lg">
+        <h4 class=" text-blue-950 text-lg font-bold mb-3">Penjualan Voucher & Kartu Perdana</h4>
+        <input type="text" wire:model.live.debounce.500ms="search" placeholder="Search .."
+            class="w-full border rounded-lg p-2 mb-1">
         <table class="table-auto w-full text-xs mb-2">
-            <thead class="bg-gray-800 text-white">
+            <thead class="bg-white text-blue-950">
                 <tr>
-                    <th class="text-left p-3">Nama Akun</th>
-                    <th class="text-right">Penambahan</th>
-                    <th class="text-right">Pengembalian</th>
-                    <th class="text-right">Sisa</th>
+                    <th class="text-left p-3">Product</th>
+                    <th class="text-center">Qty</th>
+                    <th class="text-center">Jual</th>
+                    <th class="text-center">Modal</th>
+                    <th class="text-center">Fee</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr class="border border-slate-100 odd:bg-white even:bg-slate-50">
-                    <td class="text-left p-2">Cash</td>
-                    <td class="text-left">Cash</td>
-                    <td class="text-left">Cash</td>
-                    <td class="text-left">Cash</td>
+                @foreach ($sales as $s)
+                <tr class="border border-slate-100 odd:bg-white even:bg-blue-50">
+                    <td class="text-left p-2">{{ $s->product->name }}</td>
+                    <td class="text-right p-2">{{ $s->quantity }}</td>
+                    <td class="text-right p-2">{{ number_format($s->price) }}</td>
+                    <td class="text-right p-2">{{ number_format($s->cost) }}</td>
+                    <td class="text-right p-2">{{ number_format($s->price * $s->quantity - $s->cost * $s->quantity) }}
+                    </td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
+        {{ $sales->links() }}
     </div>
-    <div>
-        <h4>Penambahan Saldo dari pusat</h4>
-        <input type="text" wire:model="search" placeholder="Search .." class="w-full border rounded-lg p-2 mb-1">
+    <div class="bg-white p-2 rounded-lg">
+        <h4 class=" text-blue-950 text-lg font-bold mb-3">Penjualan per product</h4>
+        <input type="text" wire:model.live.debounce.500ms="search" placeholder="Search .."
+            class="w-full border rounded-lg p-2 mb-1">
         <table class="table-auto w-full text-xs mb-2">
-            <thead class="bg-gray-800 text-white">
+            <thead class="bg-white text-blue-950">
                 <tr>
-                    <th class="text-left p-3">Nama Akun</th>
-                    <th class="text-right">Penambahan</th>
-                    <th class="text-right">Pengembalian</th>
-                    <th class="text-right">Sisa</th>
+                    <th class="text-left p-3">Product</th>
+                    <th class="text-center">Qty</th>
+                    <th class="text-center">Jual</th>
+                    <th class="text-center">Modal</th>
+                    <th class="text-center">Fee</th>
                 </tr>
             </thead>
 
             <tbody>
-                <tr class="border border-slate-100 odd:bg-white even:bg-slate-50">
-                    <td class="text-left p-2">Cash</td>
-                    <td class="text-left">Cash</td>
-                    <td class="text-left">Cash</td>
-                    <td class="text-left">Cash</td>
+                @foreach ($salesGroup as $productId => $productSales)
+                @php
+                $product = $productSales->first()->product;
+                $totalFee = 0;
+                @endphp
+                @foreach ($productSales as $sale)
+                @php
+                $totalFee += ($sale->price * $sale->quantity) - ($sale->cost * $sale->quantity);
+                @endphp
+                @endforeach
+                <tr class="border border-slate-100 odd:bg-white even:bg-blue-50">
+                    <td class="text-left p-2">{{ $product->name }}</td>
+                    <td class="text-right p-2">{{ $productSales->sum('quantity') }}</td>
+                    <td class="text-right p-2">{{ number_format($productSales->sum('price')) }}</td>
+                    <td class="text-right p-2">{{ number_format($productSales->sum('cost')) }}</td>
+                    <td class="text-right p-2">{{ number_format($totalFee) }}</td>
                 </tr>
+                @endforeach
             </tbody>
         </table>
+
+
     </div>
 </div>
