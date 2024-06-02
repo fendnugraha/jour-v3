10f2ed762b991061ded9017b97fd013f2343ae09
fendnugraha <fendnugraha92@gmail.com>
2024-06-02 20:22:40 +0700

    fix modal

--- resources/views/components/dropdown-button.blade.php
+++ resources/views/components/dropdown-button.blade.php
@@ -1,6 +1,7 @@
 <div x-data="{ open: false }" @click.away="open = false" class="relative">
     <button @click="open = !open" {{ $attributes->merge(['class' => '']) }}>{{ $trigger }}</button>
-    <div x-show="open" class="bg-white text-black py-1 rounded-lg absolute top-10 text-sm mt-1 w-full border shadow-300"
+    <div x-show="open"
+        class="bg-white z-50 text-black py-1 rounded-lg absolute top-10 text-sm mt-1 w-full border shadow-300"
         x-transition:enter="transition ease-out duration-300 transform origin-top"
         x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100">
         {{ $slot }}
