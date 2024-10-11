d124f1a96534164350ebe8b53a0124655d89e98b
fendnugraha <fendnugraha92@gmail.com>
2024-10-11 19:28:53 +0700

    remove journal table administrator

--- resources/views/report/administrator.blade.php
+++ resources/views/report/administrator.blade.php
@@ -2,7 +2,7 @@
     <x-slot:title>{{ $title }}</x-slot:title>
     @livewire('report.warehouse-balance')
     @livewire('report.summary', ['warehouse_id' => Auth()->user()->warehouse_id])
-    @livewire('journal.journal-table', ['warehouse_id' => Auth()->user()->warehouse_id])
+    {{-- @livewire('journal.journal-table', ['warehouse_id' => Auth()->user()->warehouse_id]) --}}
     @livewire('report.mutation-history', ['warehouse_id' => Auth()->user()->warehouse_id])
 
 </x-layouts.app>
