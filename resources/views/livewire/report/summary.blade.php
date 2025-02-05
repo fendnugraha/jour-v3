236d1ee9a8d412f438f5af36cd27294b1e734803
fendnugraha <fendnugraha92@gmail.com>
2025-02-05 13:17:44 +0700

    fix total trx

--- resources/views/livewire/report/summary.blade.php
+++ resources/views/livewire/report/summary.blade.php
@@ -52,8 +52,8 @@
             $rvdeposit += $rv->where('trx_type', 'Deposit')->sum('amount');
             $rvLaba += $w->sumfee;
             $rvBiaya += $rv->where('trx_type', 'Pengeluaran')->sum('fee_amount');
-            $totaltrx += $rv->count() - $rv->where('trx_type', 'Pengeluaran')->count() - $rv->where('trx_type', 'Mutasi
-            Kas')->count();
+            $totaltrx += $rv->count() - $rv->where('trx_type', 'Mutasi Kas')->count() - $rv->where('trx_type',
+            'Pengeluaran')->count();
             @endphp
             <tr
                 class="border border-slate-100 odd:bg-white even:bg-blue-50 hover:bg-slate-600 hover:text-white cursor-pointer">
@@ -66,8 +66,7 @@
                 </td>
                 <td class="p-2 text-right">{{ number_format($rv->where('trx_type', 'Deposit')->sum('amount')) }}</td>
                 <td class="p-2 text-right">{{ number_format($rv->count() - $rv->where('trx_type',
-                    'Pengeluaran')->count() - $rv->where('trx_type', 'Mutasi
-                    Kas')->count()) }}</td>
+                    'Pengeluaran')->count() - $rv->where('trx_type', 'Mutasi Kas')->count()) }}</td>
                 <td class="p-2 text-right text-red-600">
                     {{ number_format(-$rv->where('trx_type', 'Pengeluaran')->sum('fee_amount')) }}
                 </td>
